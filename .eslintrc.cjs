4cd5e61ab86e3e3b77cf2639e557c8a05ff9043c
chikwado17 <chikwadonworie@gmail.com>
2024-04-16 12:46:35 +0100

    mockup design completed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,8 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
-  rules: {
-    "react/prop-types": "off",
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
-};
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
+
+//https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslint()],
+});
